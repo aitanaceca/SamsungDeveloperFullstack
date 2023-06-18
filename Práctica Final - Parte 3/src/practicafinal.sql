b27375b0506eb776db64098a14022f2150285b58
aitanaceca <aitanaceca@gmail.com>
2023-06-18 14:41:31 +0200

    Corregido error en el archivo sql.

--- Práctica Final - Parte 3/src/practicafinal.sql
+++ Práctica Final - Parte 3/src/practicafinal.sql
@@ -13,11 +13,11 @@ CREATE TABLE alta_usuarios (
     password VARCHAR(8) NOT NULL CHECK (LENGTH('password') BETWEEN 4 AND 8) 
 );
 
-INSERT INTO alta_usuarios ("nombre", "primerApellido", "segundoApellido", "username", "email", "password") 
+INSERT INTO alta_usuarios (nombre, primerApellido, segundoApellido, username, email, password) 
     VALUES ("P1", "A1P1", "A2P1", "Persona1", "persona1@prueba.com", "12345678");
-INSERT INTO alta_usuarios ("nombre", "primerApellido", "segundoApellido", "username", "email", "password") 
+INSERT INTO alta_usuarios (nombre, primerApellido, segundoApellido, username, email, password) 
     VALUES ("P2", "A1P2", "A2P2", "Persona2", "persona2@prueba.com", "12345678");
-INSERT INTO alta_usuarios ("nombre", "primerApellido", "segundoApellido", "username", "email", "password") 
+INSERT INTO alta_usuarios (nombre, primerApellido, segundoApellido, username, email, password) 
     VALUES ("P3", "A1P3", "A2P3", "Persona3", "persona3@prueba.com", "12345678");
-INSERT INTO alta_usuarios ("nombre", "primerApellido", "segundoApellido", "username", "email", "password") 
+INSERT INTO alta_usuarios (nombre, primerApellido, segundoApellido, username, email, password) 
     VALUES ("P4", "A1P4", "A2P4", "Persona4", "persona4@prueba.com", "12345678");
